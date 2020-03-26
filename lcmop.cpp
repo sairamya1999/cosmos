c1047e7d9949fb426956ff4d47a619fbd7d89c4b
sairamya1999 <57129720+sairamya1999@users.noreply.github.com>
2020-03-26 18:25:08 +0530

    Create lcmop.cpp

---
+++ lcmop.cpp
@@ -0,0 +1,25 @@
+#include <iostream>  
+using namespace std; 
+class Lcmop 
+{ 
+public: int gcd(int a, int b)
+ { 
+    if (a == 0) 
+        return b;  
+    return gcd(b % a, a);  
+ } 
+ 
+ int lcm(int a, int b)  
+ {  
+    return (a*b)/gcd(a, b);  
+ }  
+} ; 
+int main()  
+{  
+    Lcmop l; 
+    int a,b;
+    cout<<"Enter a&b values:";
+    cin>>a>>b;
+    
+    cout<<"LCM of "<<a<<" and "<<b<<" is "<<l.lcm(a, b);  
+}  
